b08560465ea1bf932bada5547d03175f4dd948ea
horvathaa <ahorvath@andrew.cmu.edu>
2020-05-25 15:25:04 -0400

    gray out custom time range + mucking with filter stuff sigh

--- src/pages/Sidebar/containers/Filter/Filter.jsx
+++ src/pages/Sidebar/containers/Filter/Filter.jsx
@@ -16,7 +16,6 @@ const filterToggle = React.forwardRef(({ children, onClick }, ref) => (
 ));
 
 
-
 class Filter extends React.Component {
     selection = {
         siteScope: ['onPage'],
@@ -26,6 +25,26 @@ class Filter extends React.Component {
         archive: null,
         tags: []
     }
+    // _forceOpen = true;
+
+    state = {
+        menuOpen: false
+    }
+
+
+    // dropdownToggle(newValue) {
+    //     console.log('in dropdownToggle');
+    //     console.log(newValue);
+    //     console.log('this._forceopen', this._forceOpen);
+    //     if (this._forceOpen) {
+    //         this.setState({ menuOpen: true });
+    //         this._forceOpen = false;
+    //     } else {
+    //         this.setState({ menuOpen: newValue });
+    //     }
+    // }
+
+
 
     componentDidMount() {
         chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
@@ -60,7 +79,9 @@ class Filter extends React.Component {
             else {
                 this.selection.annoType = [];
             }
+            this._forceOpen = true;
         }
+        this._forceOpen = true;
         if (eventKey.includes('siteScope')) {
             let choice = eventKey.substring(eventKey.indexOf(':') + 1, eventKey.length)
             if (this.selection.siteScope.includes(choice)) {
@@ -68,6 +89,7 @@ class Filter extends React.Component {
             } else {
                 this.selection.siteScope.push(choice);
             }
+            this._forceOpen = true;
             // this.selection.siteScope = eventKey.substring(eventKey.indexOf(':') + 1, eventKey.length);
         }
         else if (eventKey.includes('userScope')) {
@@ -77,6 +99,7 @@ class Filter extends React.Component {
             } else {
                 this.selection.userScope.push(choice);
             }
+            this._forceOpen = true;
         }
         else if (eventKey.includes('annoType')) {
             let choice = eventKey.substring(eventKey.indexOf(':') + 1, eventKey.length)
@@ -85,81 +108,82 @@ class Filter extends React.Component {
             } else {
                 this.selection.annoType.push(choice);
             }
+            this._forceOpen = true;
         }
         else if (eventKey.includes('timeRange')) {
             this.selection.timeRange = eventKey.substring(eventKey.indexOf(':') + 1, eventKey.length)
+            this._forceOpen = true;
         }
         this.props.applyFilter(this.selection);
 
     }
+    // open={this.state.menuOpen} onToggle={val => this.dropdownToggle(val)} open={this.state.menuOpen}
 
     render() {
         return (
             <React.Fragment>
-                <Dropdown className="Filter">
+                <Dropdown className="Filter" >
                     <Dropdown.Toggle as={filterToggle} id="dropdown-basic">
 
                     </Dropdown.Toggle>
                     <Dropdown.Menu >
-                        {/* &nbsp;{this.props.filterAnnotationLength()} annotations
-                    <Dropdown.Divider /> */}
-                    &nbsp;Site scope
-                    <Dropdown.Item as="button" eventKey="siteScope:onPage" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        &nbsp;Site scope
+                    <Dropdown.Item as="button" toggle={false} eventKey="siteScope:onPage" onSelect={eventKey => this.updateSelected(eventKey)}>
                             On page {this.selection.siteScope.includes('onPage') ? ("✓") : (null)}
                         </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="siteScope:anchorToPage" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        <Dropdown.Item as="button" toggle={false} eventKey="siteScope:anchorToPage" onSelect={eventKey => this.updateSelected(eventKey)}>
                             Anchored to Page {this.selection.siteScope.includes('anchorToPage') ? ("✓") : (null)}
                         </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="siteScope:acrossWholeSite" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        <Dropdown.Item as="button" toggle={false} eventKey="siteScope:acrossWholeSite" onSelect={eventKey => this.updateSelected(eventKey)}>
                             Across whole site {this.selection.siteScope.includes('acrossWholeSite') ? ("✓") : (null)}
                         </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="siteScope:anchorToAllSitePages" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        <Dropdown.Item as="button" toggle={false} eventKey="siteScope:anchorToAllSitePages" onSelect={eventKey => this.updateSelected(eventKey)}>
                             Anchored to All Site Pages {this.selection.siteScope.includes('anchorToAllSitePages') ? ("✓") : (null)}
                         </Dropdown.Item>
                         <Dropdown.Divider />
                      &nbsp; User view
-                    <Dropdown.Item as="button" eventKey="userScope:onlyMe" onSelect={eventKey => this.updateSelected(eventKey)}>
+                    <Dropdown.Item as="button" toggle={false} eventKey="userScope:onlyMe" onSelect={eventKey => this.updateSelected(eventKey)}>
                             Only me {this.selection.userScope.includes('onlyMe') ? ("✓") : (null)}
                         </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="userScope:public" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        <Dropdown.Item as="button" toggle={false} eventKey="userScope:public" onSelect={eventKey => this.updateSelected(eventKey)}>
                             Public {this.selection.userScope.includes('public') ? ("✓") : (null)}
                         </Dropdown.Item>
                         <Dropdown.Divider />
                     &nbsp;Type of Annotation
-                    <Dropdown.Item as="button" eventKey="annoType:default" onSelect={eventKey => this.updateSelected(eventKey)}>
+                    <Dropdown.Item as="button" toggle={false} eventKey="annoType:default" onSelect={eventKey => this.updateSelected(eventKey)}>
                             Default {this.selection.annoType.includes('default') ? ("✓") : (null)}
                         </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="annoType:to-do" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        <Dropdown.Item as="button" toggle={false} eventKey="annoType:to-do" onSelect={eventKey => this.updateSelected(eventKey)}>
                             To-do {this.selection.annoType.includes('to-do') ? ("✓") : (null)}
                         </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="annoType:question" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        <Dropdown.Item as="button" toggle={false} eventKey="annoType:question" onSelect={eventKey => this.updateSelected(eventKey)}>
                             Question/Answer {this.selection.annoType.includes('question') ? ("✓") : (null)}
                         </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="annoType:highlight" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        <Dropdown.Item as="button" toggle={false} eventKey="annoType:highlight" onSelect={eventKey => this.updateSelected(eventKey)}>
                             Highlight {this.selection.annoType.includes('highlight') ? ("✓") : (null)}
                         </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="annoType:navigation" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        <Dropdown.Item as="button" toggle={false} eventKey="annoType:navigation" onSelect={eventKey => this.updateSelected(eventKey)}>
                             Navigation {this.selection.annoType.includes('navigation') ? ("✓") : (null)}
                         </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="annoType:issue" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        <Dropdown.Item as="button" toggle={false} eventKey="annoType:issue" onSelect={eventKey => this.updateSelected(eventKey)}>
                             Issue {this.selection.annoType.includes('issue') ? ("✓") : (null)}
                         </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="annoType:selectAll" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        <Dropdown.Item as="button" toggle={false} eventKey="annoType:selectAll" onSelect={eventKey => this.updateSelected(eventKey)}>
                             {this.selection.annoType.length !== 6 ? ("Select All") : ("De-select all")}
                             {/* Select All {this.selection.annoType.includes('issue') ? ("✓") : (null)} */}
                         </Dropdown.Item>
                         <Dropdown.Divider />
                     &nbsp;Time Range
-                    <Dropdown.Item as="button" eventKey="timeRange:day" onSelect={eventKey => this.updateSelected(eventKey)}>
+                    <Dropdown.Item as="button" toggle={false} eventKey="timeRange:day" onSelect={eventKey => this.updateSelected(eventKey)}>
                             Past Day {this.selection.timeRange === 'day' ? ("✓") : (null)}
                         </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="timeRange:week" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        <Dropdown.Item as="button" toggle={false} eventKey="timeRange:week" onSelect={eventKey => this.updateSelected(eventKey)}>
                             Past Week {this.selection.timeRange === 'week' ? ("✓") : (null)}
                         </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="timeRange:month" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        <Dropdown.Item as="button" toggle={false} eventKey="timeRange:month" onSelect={eventKey => this.updateSelected(eventKey)}>
                             Past Month {this.selection.timeRange === 'month' ? ("✓") : (null)}
                         </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="timeRange:custom" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        <Dropdown.Item as="button" disabled toggle={false} eventKey="timeRange:custom" /*onSelect={eventKey => this.updateSelected(eventKey)} */>
                             Custom Time Range
                         </Dropdown.Item>
                         {/* Past 6 Months {this.selection.timeRange === '6months' ? ("✓") : (null)}
@@ -167,7 +191,7 @@ class Filter extends React.Component {
                         <Dropdown.Item as="button" eventKey="timeRange:year" onSelect={eventKey => this.updateSelected(eventKey)}>
                             Past Year {this.selection.timeRange === 'year' ? ("✓") : (null)}
                         </Dropdown.Item> */}
-                        <Dropdown.Item as="button" eventKey="timeRange:all" onSelect={eventKey => this.updateSelected(eventKey)}>
+                        <Dropdown.Item as="button" toggle={false} eventKey="timeRange:all" onSelect={eventKey => this.updateSelected(eventKey)}>
                             All Time {this.selection.timeRange === 'all' ? ("✓") : (null)}
                         </Dropdown.Item>
                         <Dropdown.Divider />
