21aded0e2711605b904fc11dacd617a2f8814bef
cshannon3 <conreshan@gmail.com>
2021-01-17 18:46:37 -0500

    rewrote splitReinsertText to handle formatting issues

--- src/pages/Content/helpers/AnchorEngine/AnchorDomChanges.js
+++ src/pages/Content/helpers/AnchorEngine/AnchorDomChanges.js
@@ -50,19 +50,41 @@ export const removeTempHighlight = () => {
 }
 
 
-
+//1. Better matching of strings when faced with formatting issues
 //Splits text in node and calls callback action to preform on middle node
 var splitReinsertText = function (node, substring, callback) {
-    node.data.replace(substring, function (all) {
-        var args = [].slice.call(arguments),
-            offset = args[args.length - 2],
-            newTextNode = node.splitText(offset);
-        newTextNode.data = newTextNode.data.substr(all.length);
+    console.log("splitReinsertText");
 
-        callback.apply(window, [node].concat(args));
+    function formatText(string) {
+        return string.trim().replace(/\n/g, " ").replace(/[ ][ ]+/g, " ");
+    }
+    // Needed to format test strings to make sure matches are actually found
+    let substringText1 = formatText(substring.toString());
+    let formatted = formatText(node.data.toString());
+
+    function findTrueOffset(offset) {
+        if (offset === 0 || (formatted.length === node.data.length && formatted === node.data)) {
+            return offset;
+        }
+        let original = offset;
+        while (node.data.length > offset
+            && formatText(node.data.substring(0, offset)) != formatText(formatted.substring(0, original))) {
+            offset += 1;
+        }
+        return offset;
+    }
+
+    function handleReplace(match, offset, string) {
+        // Does offset change? check and set
+        let trueOffset = findTrueOffset(offset);
+        let newTextNode = node.splitText(trueOffset);
+        // should check to make sure match is same as substring
+        newTextNode.data = newTextNode.data.substr(substring.length);
+        callback(node, match, offset);
         return newTextNode;
+    }
+    formatted.replace(substringText1, handleReplace);
 
-    });
 }
 
 export function anchorClick(e) {
@@ -130,3 +152,12 @@ export const removeSpans = (collection) => {
 function getPathFromUrl(url) {
     return url.split(/[?#]/)[0];
 }
+
+    // if (formatted.length !== node.data.length || formatted !== node.data) {
+    //       // Something to align Texts to find actual offset if lengths are different
+    //       //TODO, need to figure out if change affects substring and if so set an offset
+    //     console.log("Need format check");
+    //     //console.log(formatted.length, formatted);
+    //     //console.log(node.data.length, node.data);
+
+    // }
