b9ba35fdba5951d900077d268d758aab33b288d0
cshannon3 <conreshan@gmail.com>
2021-01-22 16:32:07 -0500

    added packages

---
+++ src/pages/Content/helpers/AnchorEngine/packages/simple-xpath-position/xpath.js
@@ -0,0 +1,175 @@
+import { getDocument } from '../get-document'
+
+import DOMException from './dom-exception'
+
+// https://developer.mozilla.org/en-US/docs/XPathResult
+const FIRST_ORDERED_NODE_TYPE = 9
+
+// Default namespace for XHTML documents
+const HTML_NAMESPACE = 'http://www.w3.org/1999/xhtml'
+
+
+/**
+ * Compute an XPath expression for the given node.
+ *
+ * If the optional parameter `root` is supplied, the computed XPath expression
+ * will be relative to it. Otherwise, the root element is the root of the
+ * document to which `node` belongs.
+ *
+ * @param {Node} node The node for which to compute an XPath expression.
+ * @param {Node} [root] The root context for the XPath expression.
+ * @returns {string}
+ */
+export function fromNode(node, root = null) {
+    if (node === undefined) {
+        throw new Error('missing required parameter "node"')
+    }
+
+    root = root || getDocument(node)
+
+    let path = '/'
+    while (node !== root) {
+        if (!node) {
+            let message = 'The supplied node is not contained by the root node.'
+            let name = 'InvalidNodeTypeError'
+            throw new DOMException(message, name)
+        }
+        path = `/${nodeName(node)}[${nodePosition(node)}]${path}`
+        node = node.parentNode
+    }
+    return path.replace(/\/$/, '')
+}
+
+// Get the XPath node name.
+function nodeName(node) {
+    switch (node.nodeName) {
+        case '#text': return 'text()'
+        case '#comment': return 'comment()'
+        case '#cdata-section': return 'cdata-section()'
+        default: return node.nodeName.toLowerCase()
+    }
+}
+// Get the ordinal position of this node among its siblings of the same name.
+function nodePosition(node) {
+    let name = node.nodeName
+    let position = 1
+    while ((node = node.previousSibling)) {
+        if (node.nodeName === name) position += 1
+    }
+    return position
+}
+
+// OURS
+//Creates an Xpath from a node
+export const xpathConversion = (element) => {
+    if (element.tagName == 'HTML')
+        return '/HTML[1]';
+    if (element === document.body)
+        return '/HTML[1]/BODY[1]';
+    var ix = 0;
+    var txt = 0;
+    var siblings = element.parentNode.childNodes;
+    for (var i = 0; i < siblings.length; i++) {
+        var sibling = siblings[i];
+        if (sibling === element)
+            return xpathConversion(element.parentNode) + '/' + (element.nodeType === 3 ? ('text()' + '[' + (txt + 1) + ']') : (element.tagName + '[' + (ix + 1) + ']'));
+        if (sibling.nodeType === 1 && sibling.tagName === element.tagName)
+            ix++;
+        else if (sibling.nodeType === 3) {
+            txt++
+        }
+    }
+}
+
+
+
+
+
+
+
+/**
+ * Find a node using an XPath relative to the given root node.
+ *
+ * The XPath expressions are evaluated relative to the Node argument `root`.
+ *
+ * If the optional parameter `resolver` is supplied, it will be used to resolve
+ * any namespaces within the XPath.
+ *
+ * @param {string} path An XPath String to evaluate.
+ * @param {Node} root The root context for the XPath expression.
+ * @returns {Node|null} The first matching Node or null if none is found.
+ */
+export function toNode(path, root, resolver = null) {
+    if (path === undefined) {
+        throw new Error('missing required parameter "path"')
+    }
+    if (root === undefined) {
+        throw new Error('missing required parameter "root"')
+    }
+
+    // Make the path relative to the root, if not the document.
+    let document = getDocument(root)
+    if (root !== document) path = path.replace(/^\//, './')
+
+    // Make a default resolver.
+    let documentElement = document.documentElement
+    if (resolver === null && documentElement.lookupNamespaceURI) {
+        let defaultNS = documentElement.lookupNamespaceURI(null) || HTML_NAMESPACE
+        resolver = (prefix) => {
+            let ns = { '_default_': defaultNS }
+            return ns[prefix] || documentElement.lookupNamespaceURI(prefix)
+        }
+    }
+
+    return resolve(path, root, resolver)
+}
+
+// Find a single node with XPath `path`
+function resolve(path, root, resolver) {
+    try {
+        // Add a default value to each path part lacking a prefix.
+        let nspath = path.replace(/\/(?!\.)([^\/:\(]+)(?=\/|$)/g, '/_default_:$1')
+        return platformResolve(nspath, root, resolver)
+    } catch (err) {
+        return fallbackResolve(path, root)
+    }
+}
+
+
+// Find a single node with XPath `path` using the simple, built-in evaluator.
+function fallbackResolve(path, root) {
+    let steps = path.split("/")
+    let node = root
+    while (node) {
+        let step = steps.shift()
+        if (step === undefined) break
+        if (step === '.') continue
+        let [name, position] = step.split(/[\[\]]/)
+        name = name.replace('_default_:', '')
+        position = position ? parseInt(position) : 1
+        node = findChild(node, name, position)
+    }
+    return node
+}
+
+
+// Find a single node with XPath `path` using `document.evaluate`.
+function platformResolve(path, root, resolver) {
+    let document = getDocument(root)
+    let r = document.evaluate(path, root, resolver, FIRST_ORDERED_NODE_TYPE, null)
+    return r.singleNodeValue
+}
+
+
+// Find the child of the given node by name and ordinal position.
+function findChild(node, name, position) {
+    for (node = node.firstChild; node; node = node.nextSibling) {
+        if (nodeName(node) === name && --position === 0) break
+    }
+    return node
+}
+
+//OURS
+export const xpathToNode = (path) => {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
---
+++ src/pages/Content/helpers/AnchorEngine/packages/xpath-range.js
@@ -0,0 +1,395 @@
+//https://github.com/openannotation/xpath-range
+import { getDocument } from './get-document'
+import seek from './dom-seek'
+import * as xpath from './simple-xpath-position/xpath'
+
+const SHOW_TEXT = 4
+let allPaths = {};
+let USE_THREAD_TBODY = true;
+let USE_TABLE_TEXT = true;
+let debug = true;
+
+/*
+Better broken anchor solution
+- issue with formatting with \n new line
+- issue with new spans
+*/
+
+
+/**
+ * Convert a `Range` to a pair of XPath expressions and offsets.
+ *
+ * If the optional parameter `root` is supplied, the computed XPath expressions
+ * will be relative to it.
+ *
+ * @param {Range} range The Range to convert.
+ * @param {Node} [root] The root context for the XPath expressions.
+ * @returns {{start, startOffset, end, endOffset}}
+ */
+
+export function fromRange(range, root) {
+  let startNode = range.startContainer
+  let so = range.startOffset
+  let ec = range.endContainer
+  let eo = range.endOffset
+
+  let start = xpath.fromNode(startNode, root)
+  let end = xpath.fromNode(ec, root)
+
+  return {
+    start: start,
+    end: end,
+    startOffset: so,
+    endOffset: eo,
+  }
+}
+
+
+/**
+ * Construct a `Range` from the given XPath expressions and offsets.
+ *
+ * If the optional parameter `root` is supplied, the XPath expressions are
+ * evaluated as relative to it.
+ *
+ * @param {string} startPath An XPath expression for the start container.
+ * @param {Number} startOffset The textual offset within the start container.
+ * @param {string} endPath An XPath expression for the end container.
+ * @param {Number} endOffset The textual offset within the end container.
+ * @param {Node} [root] The root context for the XPath expressions.
+ * @returns Range
+ */
+export function toRange(startPath, startOffset, endPath, endOffset, root) {
+  let document = getDocument(root)
+
+  let sc = xpath.toNode(startPath, root)
+  if (sc === null) return notFound('start')
+
+  let si = document.createNodeIterator(sc, SHOW_TEXT)
+  // Issue with this 
+  let so = startOffset - seek(si, startOffset)
+  sc = si.referenceNode
+  if (!si.pointerBeforeReferenceNode) {
+    if (so > 0) return indexSize('start')
+    so += sc.length
+  }
+
+  let ec = xpath.toNode(endPath, root)
+  if (ec === null) return notFound('end')
+
+  let ei = document.createNodeIterator(ec, SHOW_TEXT)
+  let eo = endOffset - seek(ei, endOffset)
+
+  ec = ei.referenceNode
+  if (!ei.pointerBeforeReferenceNode) {
+    if (eo > 0) return indexSize('end')
+    eo += ec.length
+  }
+
+  let range = document.createRange()
+  range.setStart(sc, so);
+  range.setEnd(ec, eo);
+
+  return range
+
+  function notFound(which) {
+    let error = `The ${which} node was not found.`,
+      name = 'NotFoundError';
+    console.error(name, error);
+    return false
+  }
+
+  function indexSize(which) {
+    let error = `There is no text at the requested ${which} offset.`,
+      name = 'IndexSizeError';
+    console.error(name, error);
+    return false
+  }
+}
+
+
+/**
+ * Construct a `Range` from the given XPath expressions and offsets.
+ *
+ * If the optional parameter `root` is supplied, the XPath expressions are
+ * evaluated as relative to it.
+ *
+ * @param {string} startPath An XPath expression for the start container.
+ * @param {Number} startOffset The textual offset within the start container.
+ * @param {string} endPath An XPath expression for the end container.
+ * @param {Number} endOffset The textual offset within the end container.
+ * @param {Node} [root] The root context for the XPath expressions.
+ * @param {string} [matchContent] The match string.
+ * @returns Range
+ * 
+ *  Scenarios
+ *  - Content Not Found
+ * 
+ */
+
+
+export function toRangeNew(startPath, startOffset, endPath, endOffset, root, matchContent) {
+
+  let startNode, endNode;
+  startNode = xpath.toNode(startPath, root);
+  endNode = xpath.toNode(endPath, root);
+  if (startNode !== null && endNode !== null) {
+    console.log("Good");
+    let out;
+    try {
+      out = toRange(startPath, startOffset, endPath, endOffset, root);
+      console.log(out);
+    } catch (e) {
+      out = toRangeMissing(startPath, startOffset, endPath, endOffset, root, matchContent);
+    }
+    return out;
+  }
+  return toRangeMissing(startPath, startOffset, endPath, endOffset, root, matchContent);
+
+}
+// 
+
+//Find Node Containing Match String
+// Get an ordered list of text nodes in parent
+//Walk nodes, adding txt until find match
+//...last match is end node
+// Find end node offset
+// Walk nodes again, this time removing content until no longer match 
+// One before no longer match is startNode
+// Find start node offset;
+// return range;
+
+function toRangeMissing(startPath, startOffset, endPath, endOffset, root, matchContent) {
+  let startNode, startIterator, so, endNode, endIterator, eo, sharedParentPath;
+  //Find Node Containing Match String
+  console.log("toRangeMissing")
+  let [parentPath, parentRange] = findParentWithMatch(startPath, root, formatText(matchContent));
+  let nodes = getNodesInRange(parentRange)
+  console.log(nodes);
+  nodes = nodes.filter(function (element) {
+    return element.nodeType === 3 && element.data.trim() !== "";
+  });
+
+  console.log(nodes);
+  if (nodes.length === 0) return;
+  if (nodes.length === 1) { console.log("TODO"); return; }
+
+  let txt = "", nodeContent, formatted, done = false, j = 0;
+  let nodeIndex = 0;
+  while (nodeIndex < nodes.length && !done) {
+    let node = nodes[nodeIndex];
+    nodeContent = node.data;
+    formatted = nodeContent.toString().replace(/\n/g, " ").replace(/[ ][ ]+/g, " ");
+    txt += nodeContent;
+    if (txt.includes(matchContent)) {
+      // last match is end node
+      endNode = node;
+      // let nodeLen = nodeContent.length;
+      // let overallLen = txt.length;
+      // let offsetLen = txt.indexOf(matchContent) + matchContent.length;
+      let i = 0;
+      while (i < nodeContent.length && matchContent.includes(nodeContent.substr(0, i))) {
+        i++;
+      }
+      eo = i;
+      done = true;
+    };
+    nodeIndex += 1;
+  }
+  done = false; let t2 = "";
+  while (nodeIndex > 0 && !done) {
+    let node = nodes[nodeIndex];
+    nodeContent = node.data;
+    formatted = nodeContent.toString().replace(/\n/g, " ").replace(/[ ][ ]+/g, " ");
+    t2 = nodeContent + t2;
+    if (!t2.includes(matchContent)) {
+      // last match is end node
+      startNode = nodes[nodeIndex - 1];
+      nodeContent = startNode.data;
+      // let nodeLen = nodeContent.length;
+      // let overallLen = txt.length;
+      // let offsetLen = txt.indexOf(matchContent) + matchContent.length;
+      let i = nodeContent.length - 2;
+      while (i > 0 && matchContent.includes(nodeContent.substr(i))) {
+        i--;
+      }
+      so = i;
+      done = true;
+    };
+    nodeIndex -= 1;
+  }
+
+  let range = document.createRange()
+  range.setStart(startNode, so)
+  range.setEnd(endNode, eo)
+  return range;
+}
+
+
+const findParentWithMatch = (startPath, root, matchString) => {
+  console.log(matchString);
+  let startTime = new Date().getTime();
+  let startNode = xpath.toNode(startPath, root);
+  let maxLoops = 4; let loops = 0;
+  while ((startNode === undefined || startNode == null) && loops < maxLoops) {
+    startPath = parentPath(startPath);
+    startNode = xpath.toNode(startPath, root);
+    loops += 1;
+  }
+  if (loops === maxLoops) { console.log("Couldn't find close XPath"); return; }
+
+  let currentPath = startPath, currentNode = startNode;
+  let nodeContent, outRange;
+  if (currentPath in allPaths) {
+    nodeContent = allPaths[currentPath].content;
+  } else[nodeContent, outRange] = getNodeSelectionTextRange(currentNode, false, matchString);
+  loops = 0;
+
+  while (!nodeContent.includes(matchString) && loops < maxLoops) {
+    //console.log(nodeContent);
+    currentPath = parentPath(currentPath);
+    if (currentPath in allPaths) {
+      currentNode = allPaths[currentPath].node;
+      nodeContent = allPaths[currentPath].content;
+      outRange = allPaths[currentPath].range;
+    } else {
+      currentNode = xpath.toNode(currentPath, root);
+      [nodeContent, outRange] = getNodeSelectionTextRange(currentNode, false, matchString);
+      allPaths[currentPath] = {
+        path: currentPath, // xPath of item
+        content: nodeContent, // text content
+        length: nodeContent.length, // length of text
+        node: currentNode, // node document
+        range: outRange
+      };
+    }
+    //console.log(nodeContent);
+    loops += 1;
+  }
+  if (loops === maxLoops) { console.log("Couldn't find string"); return; }
+  console.log(`Found ${matchString} in ${nodeContent}`);
+  console.log(`Parent Xpath ${currentPath}`);
+
+  let finishTime = new Date().getTime();
+  console.log("Path traversal took " + (finishTime - startTime) + " ms.");
+  console.log(outRange);
+  // Restricted -> don't save dom documents, just the content
+  //if (debug) { console.log("allPaths"); console.log(allPaths); }
+  return [currentPath, outRange];
+}
+
+//get nodes under a range
+export const getNodesInRange = (range) => {
+  var start = range.startContainer;
+  var end = range.endContainer;
+  var commonAncestor = range.commonAncestorContainer;
+  var nodes = [];
+  var node;
+
+  // walk parent nodes from start to common ancestor
+  if (start != commonAncestor) {
+    for (node = start.parentNode; node; node = node.parentNode) {
+      nodes.push(node);
+      if (node == commonAncestor)
+        break;
+    }
+  }
+  nodes.reverse();
+  // walk children and siblings from start until end is found
+  for (node = start; node; node = getNextNode(node)) {
+    nodes.push(node);
+    if (node == end)
+      break;
+  }
+  return nodes;
+}
+
+//gets selection of node, returns it;
+function getNodeSelectionTextRange(node, shouldRestoreSelection, match) {
+  var sel, text;
+  if (shouldRestoreSelection == null) { shouldRestoreSelection = true; }
+  if (shouldRestoreSelection) { saveSelection(); }
+
+  if (debug) console.log("Select Node");
+  sel = selectNode2(node);
+  text = sel.toString().trim().replace(/\n/g, " ").replace(/[ ][ ]+/g, " ");
+
+  if (shouldRestoreSelection) { restoreSelection(); }
+  console.log(text)
+  return [text, sel.getRangeAt(0)];
+};
+
+//Gets node text selection
+function selectNode2(node) {
+  var children, range, sel, sn, _ref;
+  sel = window.getSelection();
+  sel.removeAllRanges();
+  range = window.document.createRange();
+  if (USE_THREAD_TBODY && node.nodeType === Node.ELEMENT_NODE && ((_ref = node.tagName.toLowerCase()) === "thead" || _ref === "tbody") && node.hasChildNodes()) {
+    children = node.childNodes;
+    range.setStartBefore(children[0]);
+    range.setEndAfter(children[children.length - 1]);
+    sel.addRange(range);
+  } else {
+    if (USE_TABLE_TEXT && node.nodeType === Node.TEXT_NODE && node.parentNode.tagName.toLowerCase() === "table") {
+    } else {
+      range.setStartBefore(node);
+      range.setEndAfter(node);
+      sel.addRange(range);
+    }
+  }
+  return sel;
+};
+
+function formatText(string) {
+  return string.trim().replace(/\n/g, " ").replace(/[ ][ ]+/g, " ");
+}
+
+function getNextNode(node) {
+  if (node.firstChild) {
+    return node.firstChild;
+  }
+  while (node) {
+    if (node.nextSibling)
+      return node.nextSibling;
+    node = node.parentNode;
+  }
+}
+
+function saveSelection(oldRanges) {
+  var i, sel, _i, _ref, _ref1;
+  // Gets selected text from dom
+  sel = window.getSelection();
+  for (i = _i = 0, _ref = sel.rangeCount;
+    0 <= _ref ? _i < _ref : _i > _ref;
+    i = 0 <= _ref ? ++_i : --_i) {
+    oldRanges = sel.getRangeAt(i);
+  }
+  switch (sel.rangeCount) {
+    case 0:
+      return (_ref1 = oldRanges) != null ? _ref1 : oldRanges = [];
+    case 1:
+      return oldRanges = [oldRanges];
+  }
+};
+
+function restoreSelection(oldRanges) {
+  window = window;
+  var range, sel, _i, _len, _ref, _results;
+  sel = window.getSelection();
+  sel.removeAllRanges();
+  _ref = oldRanges;
+  _results = [];
+  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+    range = _ref[_i];
+    _results.push(sel.addRange(range));
+  }
+  return _results;
+};
+
+function stringStartsWith(string, prefix) {
+  return prefix === string.substr(0, prefix.length);
+};
+
+function parentPath(path) {
+  return path.substr(0, path.lastIndexOf("/"));
+};
