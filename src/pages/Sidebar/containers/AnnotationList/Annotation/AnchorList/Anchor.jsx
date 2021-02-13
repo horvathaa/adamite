4a2be6954e6463f3b74135ca30b4c10d8392e2c5
cshannon3 <conreshan@gmail.com>
2021-02-12 23:53:04 -0500

    got basic tagging, issues with auto-complete

--- src/pages/Sidebar/containers/AnnotationList/Annotation/AnchorList/Anchor.jsx
+++ src/pages/Sidebar/containers/AnnotationList/Annotation/AnchorList/Anchor.jsx
@@ -9,19 +9,126 @@ import anchorBroken from '../../../../../../assets/img/SVGs/Anchor_broken.svg';
 import Tooltip from '@material-ui/core/Tooltip';
 import '../Annotation.css';
 import './Anchor.css';
+import TagsInput from 'react-tagsinput';
+import Autosuggest from 'react-autosuggest'
 
+
+function states() {
+    return [
+        { abbr: 'AL', name: 'Alabama' },
+        { abbr: 'AK', name: 'Alaska' },
+        { abbr: 'AZ', name: 'Arizona' },
+        { abbr: 'AR', name: 'Arkansas' },
+        { abbr: 'CA', name: 'California' },
+        { abbr: 'CO', name: 'Colorado' },
+        { abbr: 'CT', name: 'Connecticut' },
+        { abbr: 'DE', name: 'Delaware' },
+        { abbr: 'FL', name: 'Florida' },
+        { abbr: 'GA', name: 'Georgia' },
+        { abbr: 'HI', name: 'Hawaii' },
+        { abbr: 'ID', name: 'Idaho' },
+        { abbr: 'IL', name: 'Illinois' },
+        { abbr: 'IN', name: 'Indiana' },
+        { abbr: 'IA', name: 'Iowa' },
+        { abbr: 'KS', name: 'Kansas' },
+        { abbr: 'KY', name: 'Kentucky' },
+        { abbr: 'LA', name: 'Louisiana' },
+        { abbr: 'ME', name: 'Maine' },
+        { abbr: 'MD', name: 'Maryland' },
+        { abbr: 'MA', name: 'Massachusetts' },
+        { abbr: 'MI', name: 'Michigan' },
+        { abbr: 'MN', name: 'Minnesota' },
+        { abbr: 'MS', name: 'Mississippi' },
+        { abbr: 'MO', name: 'Missouri' },
+        { abbr: 'MT', name: 'Montana' },
+        { abbr: 'NE', name: 'Nebraska' },
+        { abbr: 'NV', name: 'Nevada' },
+        { abbr: 'NH', name: 'New Hampshire' },
+        { abbr: 'NJ', name: 'New Jersey' },
+        { abbr: 'NM', name: 'New Mexico' },
+        { abbr: 'NY', name: 'New York' },
+        { abbr: 'NC', name: 'North Carolina' },
+        { abbr: 'ND', name: 'North Dakota' },
+        { abbr: 'OH', name: 'Ohio' },
+        { abbr: 'OK', name: 'Oklahoma' },
+        { abbr: 'OR', name: 'Oregon' },
+        { abbr: 'PA', name: 'Pennsylvania' },
+        { abbr: 'RI', name: 'Rhode Island' },
+        { abbr: 'SC', name: 'South Carolina' },
+        { abbr: 'SD', name: 'South Dakota' },
+        { abbr: 'TN', name: 'Tennessee' },
+        { abbr: 'TX', name: 'Texas' },
+        { abbr: 'UT', name: 'Utah' },
+        { abbr: 'VT', name: 'Vermont' },
+        { abbr: 'VA', name: 'Virginia' },
+        { abbr: 'WA', name: 'Washington' },
+        { abbr: 'WV', name: 'West Virginia' },
+        { abbr: 'WI', name: 'Wisconsin' },
+        { abbr: 'WY', name: 'Wyoming' }
+    ]
+}
 // helper method from
 // https://stackoverflow.com/questions/2540969/remove-querystring-from-url
 function getPathFromUrl(url) {
     return url.split(/[?#]/)[0];
 }
+function getSuggestionValue(suggestion) {
+    return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+    console.log(suggestion);
+    return (
+        <span>{suggestion.name}</span>
+    );
+}
+function escapeRegexCharacters(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+function getSuggestions(value) {
+
+    const escapedValue = escapeRegexCharacters(value.trim());
+
+    if (escapedValue === '') {
+        return [];
+    }
+    const inputLength = escapedValue.length
+    const regex = new RegExp('^' + escapedValue, 'i');
+    let suggestions = states().filter((state) => {
+        return state.name.toLowerCase().slice(0, inputLength) === escapedValue
+    })
+    return suggestions;
+}
 
 class Anchor extends Component {
 
     state = {
-        broken: false
+        broken: false,
+        hovering: false,
+        tags: this.props.tags !== undefined && this.props.tags.length === 0 ? [] : this.props.tags,
+        suggestions: [],
+    }
+    updateData = () => {
+        let { tags } = this.props;
+        this.setState({ tags })
+    }
+    componentDidMount() {
+        document.addEventListener('keydown', this.keydown, false);
+        this.updateData();
     }
 
+    componentDidUpdate(prevProps) {
+        if (prevProps.tags !== this.props.tags) { this.updateData(); }
+    }
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.keydown, false);
+    }
+    tagsHandleChange = (newTag) => {
+        this.setState({ tags: newTag })
+    }
+    handleChange(tags) {
+        this.setState({ tags })
+    }
     handleOnLocalOnClick = () => {
         if (this.props.id === null) {
             return;
@@ -58,6 +165,7 @@ class Anchor extends Component {
                 );
             }
         });
+        this.setState({ hovering: true });
     }
 
     handleOnLocalOnMouseLeave = () => {
@@ -77,6 +185,7 @@ class Anchor extends Component {
                 );
             }
         });
+        this.setState({ hovering: false });
     }
 
     // handleOnClick = ()
@@ -86,8 +195,47 @@ class Anchor extends Component {
     }
 
     render() {
-        const { currentUrl, collapsed, url, anchorContent, tags, pageAnchor, brokenAnchor } = this.props;
+        const { currentUrl, collapsed, url, anchorContent, pageAnchor, brokenAnchor } = this.props;
+        const { tags } = this.state;
+        function autocompleteRenderInput({ addTag, ...props }) {
+            console.log("ppp")
+            //console.log(props)
+            const handleOnChange = (e, { newValue, method }) => {
+                if (method === 'enter') {
+                    e.preventDefault()
+                } else {
+                    props.onChange(e)
+                }
+            }
 
+            const inputValue = (props.value && props.value.trim().toLowerCase()) || ''
+            const inputLength = inputValue.length
+            let suggestions = states().filter((state) => {
+                return state.name.toLowerCase().slice(0, inputLength) === inputValue;
+            })
+            suggestions = getSuggestions(inputValue)
+            console.log(suggestions);
+
+            return (
+                <Autosuggest
+                    suggestions={suggestions}
+                    shouldRenderSuggestions={(value) => value && value.trim().length > 0}
+                    getSuggestionValue={getSuggestionValue}
+                    renderSuggestion={renderSuggestion}
+                    inputProps={{ ...props, onChange: handleOnChange }}
+                    onSuggestionSelected={(e, { suggestion }) => {
+                        addTag(suggestion.name)
+                    }}
+                    onSuggestionsClearRequested={() => { }}
+                    onSuggestionsFetchRequested={(value) => {
+                        console.log(value)
+                        // this.setState({
+                        //     suggestions: getSuggestions(value)
+                        // })
+                    }}
+                />
+            )
+        }
         let anchorIcon;
 
         if (pageAnchor) {
@@ -126,6 +274,7 @@ class Anchor extends Component {
             )
         }
         else {
+            let textClass = collapsed ? "AnchorTextContainer" : "AnchorTextContainerExpanded";
             return (
                 <div
                     className={classNames({
@@ -139,12 +288,28 @@ class Anchor extends Component {
                     <div className="AnchorIconContainer">
                         {anchorIcon}
                     </div>
-                    {url.includes(currentUrl) && !pageAnchor ? (
-                        <div className="AnchorTextContainer">
-                            {anchorContent}
+                    {url.includes(currentUrl) && !pageAnchor ? this.state.hovering ? (
+                        <div className={textClass}>
+                            <div className={textClass}>
+                                {anchorContent}
+                            </div>
+                            <TagsInput renderInput={autocompleteRenderInput} value={tags ?? []} onChange={this.handleChange.bind(this)} />
                         </div>
                     ) : (
-                        <div className="AnchorTextContainer">
+
+                        <div className={textClass}>
+                            {anchorContent}
+                            {tags && tags[0] &&
+                                <div className="AnchorTagMenu">
+                                    <div className="AnchorTag">
+                                        {tags[0]}
+                                    </div>
+
+                                </div>
+                            }
+                        </div>
+                    ) : (
+                        <div className={textClass}>
                             {anchorContent}
                             <div className="AnchorUrlContainer" onClick={this.handleExternalAnchor}>
                                 {url}
@@ -160,3 +325,50 @@ class Anchor extends Component {
 }
 
 export default Anchor;
+
+/*
+onlyUnique={true}
+ {tags ?
+:
+
+                                <div className="AnchorTagMenu">
+                                    <div className="AnchorTag">
+                                        add tag
+                                </div>
+                                </div>
+                            }
+ updateData = () => {
+        let { tags, annotationContent, annotationType } = this.props;
+
+        this.setState({
+            tags, elseContent: annotationContent, annotationType: annotationType === undefined ? "Default" : annotationType
+        })
+    }
+    componentDidMount() {
+        document.addEventListener('keydown', this.keydown, false);
+        this.updateData();
+    }
+
+   componentDidUpdate(prevProps) {
+        if (prevProps.tags !== this.props.tags || prevProps.annotationContent !== this.props.annotationContent || prevProps.type !== this.props.type) {
+            this.updateData();
+        }
+    }
+   componentWillUnmount() {
+        document.removeEventListener('keydown', this.keydown, false);
+    }
+
+   annotationTagHandler = event => {
+
+    }
+    tagsHandleChange = (newTag) => {
+        this.setState({ tags: newTag })
+    }
+<div className="Tag-Container">
+                        <div className="row">
+                            <div className="TextareaContainer">
+                                <TagsInput value={tags} onChange={this.tagsHandleChange} onlyUnique={true} />
+                            </div>
+                        </div>
+                    </div>
+*/
